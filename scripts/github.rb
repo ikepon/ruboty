55ab38b9ab0eff865c87009fbf4afdf054e8815a
ikepon <tatsuyanoheya@gmail.com>
2017-03-02 17:25:58 +0900

    lightweightタグを作成

--- scripts/github.rb
+++ scripts/github.rb
@@ -5,6 +5,8 @@ module Ruboty
 
       on(/branches/i, name: 'branches', description: 'ブランチ一覧を表示')
 
+      on(/create lw_tag (?<name>.+) (?<branch>.+)/i, name: 'create_ref', description: '指定ブランチから lightweight tag を作成(ブランチがない場合はmasterから作成)')
+
       def issues(message)
         issues = client.issues(repo)
         issue_names = "最新の20件だ！\n"
@@ -22,6 +24,25 @@ module Ruboty
         branch_names = "ブランチ一覧:\n"
 
         message.reply(branch_names + git_branches.join("\n"))
+      end
+
+      def create_ref(message)
+        tag_name = message[:name]
+        branches = client.branches(repo)
+        original_branch_sha1 = ''
+
+        branches.each do |branch|
+          if branch.name == message[:branch]
+            original_branch_sha1 = branches.first.commit.sha
+            break
+          elsif branch.name == 'master'
+            original_branch_sha1 = branches.first.commit.sha
+          end
+        end
+
+        client.create_ref(repo, 'tags/' + tag_name, original_branch_sha1)
+
+        message.reply('lightweight tag を作成しました')
       end
     end
   end
